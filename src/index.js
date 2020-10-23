cc91fc2b78a854532f41e24955c3bbeeb07710f4
Andrey Zatochniy <andreyz11288@gmail.com>
2020-10-23 22:51:19 +0300

    работает

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import changeTheme from './js/changeTheme'
+import './images/sprite.svg'
+import './styles.css'
+import './js/lazy.js'
+import menu from './store/menu.json';
+import newStringEl from './js/addMarkup.js'
+
+
+
+
+newStringEl(menu)
+changeTheme()
